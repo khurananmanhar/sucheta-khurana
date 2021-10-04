6042c21328df375db58884f062f9ad8ad7adece2
khuranamanhar <72623458+khurananmanhar@users.noreply.github.com>
2021-10-03 20:51:04 -0400

    Update Closing.js

--- src/components/Closing/Closing.js
+++ src/components/Closing/Closing.js
@@ -5,7 +5,7 @@ class Closing extends React.Component{
     render(){
         return(
             <div className="container closingContainer">
-                <h3 className="closing">Think I’d fit your project? <br/> Get in touch</h3>
+                <h3 className="closing">Think I’d fit your project? <br/> Get in touch ></h3>
                 <div className="contact">
                     <p>Say Hello!</p>
                     <p><a href="mailto:sucheta.khurana@gmail.com">Email,</a> <a href="https://www.instagram.com/sucheta.khurana/" target="_blank" rel="noreferrer">Instagram,</a> <a href="https://www.linkedin.com/in/suchetakhurana/" target="_blank" rel="noreferrer">LinkedIn,</a> <a href="https://twitter.com/sxcheta" target="_blank" rel="noreferrer">Twitter</a></p>
