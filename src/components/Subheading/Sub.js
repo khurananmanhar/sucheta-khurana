9a1c6158ec8a24c796f056255059aff28921591a
khuranamanhar <72623458+khurananmanhar@users.noreply.github.com>
2021-10-04 17:30:10 -0400

    updates given by didi

--- src/components/Subheading/Sub.js
+++ src/components/Subheading/Sub.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './Sub.css'
+
 class Sub extends React.Component{
     render(){
         return(
             <div className="container">
                 <h3 className="subHeading selected">Selected Work</h3>
+                <p className="downArrow">˅</p>
             </div>
         )
     }
